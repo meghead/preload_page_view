fff3711669e9c20f620969060785234b68bdae53
meghead <megthing@gmail.com>
2025-03-23 19:04:21 +0000

    Update page.dart
    Null safey

--- example/lib/page.dart
+++ example/lib/page.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 class DemoPage extends StatelessWidget {
-  DemoPage(this.index);
+  DemoPage(this.index, {super.key});
   final int index;
 
   @override
   Widget build(BuildContext context) {
-    print('Page loaded: $index}');
+    print('Page loaded: $index');
 
     return Center(
       child: Text(
         'Page $index',
-        style: Theme.of(context).textTheme.display1,
+        style: Theme.of(context).textTheme.displayLarge, //displayLarge is the null safe replacement for display1.
       ),
     );
   }
