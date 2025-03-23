f20b201519db445a108423b2e68097b2f30e57d6
meghead <megthing@gmail.com>
2025-03-23 19:03:00 +0000

    Update main.dart
    null safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,13 +4,15 @@ import 'package:flutter/widgets.dart';
 import 'package:preload_page_view/preload_page_view.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'PreloadPageView Demo',
       home: PreloadPageViewDemo(),
     );
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
 }
 
 class PreloadPageViewDemo extends StatefulWidget {
-  PreloadPageViewDemo({Key key}) : super(key: key);
+  const PreloadPageViewDemo({super.key});
 
   @override
   _PreloadPageViewState createState() => _PreloadPageViewState();
@@ -28,17 +30,19 @@ class _PreloadPageViewState extends State<PreloadPageViewDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("PreloadPageView Demo"),
-        ),
-        body: Container(
-            child: PreloadPageView.builder(
+      appBar: AppBar(
+        title: const Text("PreloadPageView Demo"),
+      ),
+      body: Container(
+        child: PreloadPageView.builder(
           preloadPagesCount: 5,
           itemBuilder: (BuildContext context, int position) => DemoPage(position),
           controller: PreloadPageController(initialPage: 1),
           onPageChanged: (int position) {
             print('page changed. current: $position');
           },
-        )));
+        ),
+      ),
+    );
   }
 }
